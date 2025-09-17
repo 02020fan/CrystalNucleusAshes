f614009d6347a1e8c6fbb1c6a4bc747cf99f0a7e
YYYYYY <2960935247@qq.com>
2025-09-17 20:06:23 +0800

    YY第八次提交
    大厅进一步完善

--- Script/UI/Lobby/MainLobby/Item/UMG_Lobby_PlayerInfo.lua
+++ Script/UI/Lobby/MainLobby/Item/UMG_Lobby_PlayerInfo.lua
@@ -1,21 +1,42 @@
 ---@class UMG_Lobby_PlayerInfo_C:UUserWidget
 ---@field Common_Avatar_BP Common_Avatar_BP_C
+---@field Gendar UWidgetSwitcher
+---@field PlayerLevel UTextBlock
+---@field PlayerName UTextBlock
 ---@field ProgressBar_Exp UProgressBar
 ---@field TextBlock_0 UTextBlock
----@field TextBlock_1 UTextBlock
 ---@field TextBlock_NowExp UTextBlock
----@field TextBlock_PlayerLevel UTextBlock
----@field TextBlock_PlayerName UTextBlock
 ---@field TextBlock_TargetExp UTextBlock
----@field WidgetSwitcher_Gendar UWidgetSwitcher
+---@field UID UTextBlock
 --Edit Below--
-local UMG_Lobby_PlayerInfo = { bInitDoOnce = false } 
+local UMG_Lobby_PlayerInfo = {} 
 
---[==[ Construct
+
 function UMG_Lobby_PlayerInfo:Construct()
-	
+
+    print("UMG_Lobby_PlayerInfo:Construct");
+	local PC=UGCGameSystem.GetLocalPlayerController();
+
+    print("UMG_Lobby_PlayerInfo:PC");
+
+    local PS=UGCGameSystem.GetPlayerStateByPlayerController(PC);
+
+    print("UMG_Lobby_PlayerInfo:PS");
+
+    if PS then
+
+        self.Common_Avatar_BP:InitView(1,UGCGameSystem.GetUIDByPlayerState(PS),PS.IconURL,PS.PlatformGender,PS.SegmentLevel,PS.PlayerLevel,true,true);
+
+        print("PS.Gender"..PS.PlatformGender);
+
+        self.PlayerName:SetText(PS.PlayerName or "nil");
+        
+        self.UID:SetText((UGCGameSystem.GetUIDByPlayerState(PS) or "nil"));
+
+    end
+
 end
--- Construct ]==]
+
 
 -- function UMG_Lobby_PlayerInfo:Tick(MyGeometry, InDeltaTime)
 
@@ -25,24 +46,24 @@ end
 
 -- end
 
-function UMG_Lobby_PlayerInfo:OnUpdate(Data)
+-- function UMG_Lobby_PlayerInfo:OnUpdate(Data)
     
-    self.Data = Data
+--     self.Data = Data
 
-    ---@type FPlayerAccountInfo
-    local PlayerAccountInfo = self.Data.PlayerAccountInfo
+--     ---@type FPlayerAccountInfo
+--     local PlayerAccountInfo = self.Data.PlayerAccountInfo
 
-    self.TextBlock_PlayerName:SetText(PlayerAccountInfo.PlayerName or "nil")
-    self.TextBlock_PlayerLevel:SetText("Lv. " .. self.Data.Level or 0)
+--     self.TextBlock_PlayerName:SetText(PlayerAccountInfo.PlayerName or "nil")
+--     self.TextBlock_PlayerLevel:SetText("Lv. " .. self.Data.Level or 0)
 
-    self.TextBlock_NowExp:SetText(tostring(self.Data.NowExp or 2))
-    self.TextBlock_TargetExp:SetText(tostring(self.Data.TargetExp or 10))
-    self.ProgressBar_Exp:SetPercent(self.Data.ExpRatio or 0.2)
+--     self.TextBlock_NowExp:SetText(tostring(self.Data.NowExp or 2))
+--     self.TextBlock_TargetExp:SetText(tostring(self.Data.TargetExp or 10))
+--     self.ProgressBar_Exp:SetPercent(self.Data.ExpRatio or 0.2)
 
-    -- 刷新头像
-    self.Common_Avatar_BP:InitView(1, PlayerAccountInfo.UID, PlayerAccountInfo.IconURL, PlayerAccountInfo.Gender,
-            PlayerAccountInfo.SegmentLevel, PlayerAccountInfo.PlayerLevel, true, true)
+--     -- 刷新头像
+--     self.Common_Avatar_BP:InitView(1, PlayerAccountInfo.UID, PlayerAccountInfo.IconURL, PlayerAccountInfo.Gender,
+--             PlayerAccountInfo.SegmentLevel, PlayerAccountInfo.PlayerLevel, true, true)
             
-end
+-- end
 
 return UMG_Lobby_PlayerInfo
